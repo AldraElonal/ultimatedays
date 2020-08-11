447f8dd0e3b98ce7c51c5a0e8e9332bb98a7196c
AldraElonal <46710857+AldraElonal@users.noreply.github.com>
2020-08-11 10:55:20 +0200

    dfrdfgf

--- src/main/java/ultimatedays/hello/App.java
+++ src/main/java/ultimatedays/hello/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("SYMFONY RULES THE WORLD!");
-        
+        System.out.println("A BAS SPRING");
     }
 }
